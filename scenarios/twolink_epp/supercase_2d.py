02b167cee0b5b1815ce744007305b3b9d9f9c59a
Julius Wallnöfer <33934646+jwallnoefer@users.noreply.github.com>
2022-04-04 16:32:53 +0200

    another type in supercase

--- scenarios/twolink_epp/supercase_2d.py
+++ scenarios/twolink_epp/supercase_2d.py
@@ -6,17 +6,17 @@ path = os.path.join("scenarios", "twolink_epp")
 
 # schedules the cases of 2dplots in
 no_epp_cases = np.arange(6, 10)
-epp_case = np.arange(10, 14)
+epp_cases = np.arange(10, 14)
 
 
 with open("environment_setup.txt", "r") as f:
     environment_setup_string = f.read()
 
-for case in no_epp_cases:
-    subprocess.run(["pipenv", "run", "python",
-                    os.path.join(path, "orchestrate.py"),
-                    "--time", "0-00:30:00", "--mailtype", "FAIL",
-                    f"{case}"])
+# for case in no_epp_cases:
+#     subprocess.run(["pipenv", "run", "python",
+#                     os.path.join(path, "orchestrate.py"),
+#                     "--time", "0-00:30:00", "--mailtype", "FAIL",
+#                     f"{case}"])
 
 
 for case in epp_cases:
