bfce6a347fa0f677e847284768d2340ce82151c7
Julius Wallnöfer <julius.wallnoefer@outlook.com>
2022-04-06 23:19:14 +0200

    fixes plotting to ready

--- scenarios/twolink_epp/2d_evaluate.py
+++ scenarios/twolink_epp/2d_evaluate.py
@@ -15,7 +15,7 @@ def is_always_better(no_epp_data, epp_data):
     no_epp_series = no_epp_data["key_per_time"]
     epp_series = epp_data["key_per_time"]
     positive_idx = epp_series > 0
-    return np.all(epp_series[positive_idx] > no_epp_series[positive_idx])
+    return np.all(epp_series[positive_idx] >= no_epp_series[positive_idx])
 
 
 def extends_range(no_epp_data, epp_data):
@@ -34,18 +34,23 @@ res_better = []
 res_extend = []
 
 for no_epp_case, epp_case in zip(no_epp_cases, epp_cases):
-    no_epp_path = os.path.join(base_path,
-                               case_definition.name(no_epp_case),
-                               case_definition.subcase_name(no_epp_case),
-                               "result.csv")
-    no_epp_data = pd.read_csv(no_epp_path, index_col=0)
-    epp_path = os.path.join(base_path,
-                            case_definition.name(epp_case),
-                            case_definition.subcase_name(epp_case),
-                            "result.csv")
-    epp_data = pd.read_csv(epp_path, index_col=0)
-    res_better += [is_always_better(no_epp_data, epp_data)]
-    res_extend += [extends_range(no_epp_data, epp_data)]
+    try:
+        no_epp_path = os.path.join(base_path,
+                                   case_definition.name(no_epp_case),
+                                   case_definition.subcase_name(no_epp_case),
+                                   "result.csv")
+        no_epp_data = pd.read_csv(no_epp_path, index_col=0)
+        epp_path = os.path.join(base_path,
+                                case_definition.name(epp_case),
+                                case_definition.subcase_name(epp_case),
+                                "result.csv")
+        epp_data = pd.read_csv(epp_path, index_col=0)
+        res_better += [is_always_better(no_epp_data, epp_data)]
+        res_extend += [extends_range(no_epp_data, epp_data)]
+    except FileNotFoundError:
+        print("something didn't work at", no_epp_case, epp_case)
+        res_better += [False]
+        res_extend += [False]
     # print(case_definition.subcase_name(no_epp_case),
     #       is_always_better(no_epp_data, epp_data),
     #       extends_range(no_epp_data, epp_data))
@@ -56,12 +61,15 @@ memory_times = np.logspace(-3, 0, num=7)  # 1 ms to 1 second
 # fidelities = [0.93, 0.96]
 # memory_times = [10e-3, 100e-3]
 
-res_better = np.array(res_better, dtype=int).reshape((len(memory_times), len(fidelities)))
-res_extend = np.array(res_extend, dtype=int).reshape((len(memory_times), len(fidelities)))
+res_better = np.array(res_better, dtype=int).reshape((len(fidelities), len(memory_times)))
+res_extend = np.array(res_extend, dtype=int).reshape((len(fidelities), len(memory_times)))
 
 plt.pcolormesh(memory_times, fidelities, res_better, shading="nearest")
-# plt.yscale("log")
+plt.xscale("log")
+plt.xlim(10**-3, 1)
 plt.show()
 
 plt.pcolormesh(memory_times, fidelities, res_extend, shading="nearest")
+plt.xscale("log")
+plt.xlim(10**-3, 1)
 plt.show()
